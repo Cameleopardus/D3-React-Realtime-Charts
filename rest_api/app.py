5dbc9127249da35140fad76225cb8c742215145f
Louis M <louis@sortofaweso.me>
2021-02-23 17:59:46 -0800

    dockerize and stuff

---
+++ rest_api/app.py
@@ -0,0 +1,28 @@
+import time
+import os
+import redis
+from flask import Flask, send_from_directory
+from flask import jsonify
+import random
+
+app = Flask(__name__, static_folder='frontend/build')
+
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/data')
+def get_chart_data():
+    data = [
+        {"event_type": "type_1", "occurences": random.choice(range(0,50))},
+        {"event_type": "type_3", "occurences": random.choice(range(0,50))},
+        {"event_type": "type_4", "occurences": random.choice(range(0,50))},
+        {"event_type": "type_2", "occurences": random.choice(range(0,50))},
+    ]
+    return jsonify(data)
